46d95717edcbf93f4670abf20afbc76fa050b9b9
Henry Ning <henryhuning@gmail.com>
2021-01-04 09:15:05 +0000

    49. [Interactive Coding Exercise] Average Height 9min

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-#1-3-2021 Sunday
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
 
-fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-  print(fruit)
-  print(fruit + " Pie")
-  print(fruits)
+
+#Write your code below this row 👇
+
+number = 0
+height_sum = 0
+
+for height in student_heights:
+  height_sum = height_sum + student_heights[number]
+  number = number + 1
+
+print(height_sum/number)
+
+
