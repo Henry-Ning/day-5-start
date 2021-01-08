00030351021e1ee3dfeda927f14de09118e9c06a
Henry Ning <henryhuning@gmail.com>
2021-01-08 18:21:40 +0000

    53. [Interactive Coding Exercise] The FizzBuzz Job Interview Questi…53. [Interactive Coding Exercise] The FizzBuzz Job Interview Question 5min

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-total = 0
 for number in range(1,101):
-  if number % 2 == 0:
-    total += number
-print(total)
+  if number % 15 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("buzz")
+  else:
+    print(number)
